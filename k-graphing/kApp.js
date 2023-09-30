16511cf72918840549e19ae68821a3e04c59318c
deilann <yandoryn@gmail.com>
2023-09-30 08:19:55 -0500

    fix direct populate not removing buttons

--- k-graphing/kApp.js
+++ k-graphing/kApp.js
@@ -10,6 +10,7 @@ kView.innerHTML = k
 choiceHead.innerHTML = 'Choose Wisely'
 
 function populate() {
+    buttonContainer.textContent = ''
     choiceHead.innerHTML = 'Choose Wisely'
     weapons_info = []
     fetch(JSON_PATH)
